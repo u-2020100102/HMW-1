ca47814ba189561e2c0fd8f486e20fe9b3df0b1c
u-2020100102 <changewhilealive@gmail.com>
2024-02-28 01:36:43 +0300

    create the controller and the view

---
+++ WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    {
+        public ActionResult Display()
+        {
+            var book = new BookModel
+            {
+                Name = "Sample Book",
+                Author = "Jane Doe",
+                PageNumber = 300,
+                DueDate = DateTime.Now.AddDays(10)
+            };
+
+            ViewBag.book = book;
+            return View();
+        }
+
+    }
+}
+
---
+++ WebApplication1/Models/BookModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class BookModel
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public int DaysUntilDue => (DueDate - DateTime.Now).Days;
+    }
+}
